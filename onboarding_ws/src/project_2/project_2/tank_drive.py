705ea5650c0e9f6038f1d5c590cc234c4705e834
brclaptop <brclaptop2@gmail.com>
2026-09-15 20:28:45 -0400

    fixed stuff

--- onboarding_ws/src/project_2/project_2/tank_drive.py
+++ onboarding_ws/src/project_2/project_2/tank_drive.py
@@ -1,22 +1,48 @@
 import rclpy
 from rclpy.node import Node
 
-from geometry_msgs.msg import Twist
-from custom_interfaces.msg import ControlMessage
-from custom_interfaces.srv import AxisState
+from odrive_can.srv import AxisState
+from odrive_can.msg import ControlMessage, ControllerStatus, ODriveStatus
+from rclpy.qos import qos_profile_sensor_data
+from rclpy.qos import (
+    QoSProfile,
+    ReliabilityPolicy,
+    HistoryPolicy,
+    DurabilityPolicy,
+    LivelinessPolicy,
+    Duration,
+)
 
+
+from geometry_msgs.msg import Twist
 class TankDriveNode(Node):
 
     def __init__(self):
         super().__init__('tank_driver_node')
 
-        self.publisher_ = self.create_publisher(ControlMessage, '/odrive_axis0/control_message', 10)
+        self.qos_publish = QoSProfile(
+            history=HistoryPolicy.KEEP_LAST,
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            depth=1,
+            durability=DurabilityPolicy.VOLATILE,
+            liveliness=LivelinessPolicy.AUTOMATIC,
+            lifespan=Duration(seconds=0.1, nanoseconds=0),
+        )
+        self.ctrl_msg_qos = QoSProfile(history=HistoryPolicy.KEEP_ALL)
+
+        self.qos_sub = QoSProfile(
+            history=HistoryPolicy.KEEP_ALL
+        )
+
+        self.publisher_ = self.create_publisher(ControlMessage, '/odrive_axis0/control_message', self.ctrl_msg_qos)
         self.client = self.create_client(AxisState, '/odrive_axis0/request_axis_state')
         self.subscription = self.create_subscription(
                     Twist,
                     'cmd_vel',
                     self.joy_node_callback,
-                    10)
+                    self.qos_publish)
+
+            
 
         self.declare_parameter("max_vel", 5.0)
     
@@ -38,8 +64,8 @@ class TankDriveNode(Node):
 
         # Publish Message
         draft = ControlMessage()
-        draft.control_mode = 1
-        draft.input_mode = 2
+        draft.control_mode = 2
+        draft.input_mode = 1
         draft.input_vel = msg.linear.x * self.get_parameter("max_vel").value
 
         self.publisher_.publish(draft)
