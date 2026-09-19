69ced7df0958cb0bb7ef32f5f479be32cca11808
Sab3rson <porebawheeling@gmail.com>
2026-09-19 10:02:19 -0400

    Committing tankdrive node program

---
+++ onboarding_ws/src/onboarding_part2/onboarding_part2/TankDriveNode.py
@@ -0,0 +1,87 @@
+import rclpy
+from rclpy.node import Node
+
+from geometry_msgs.msg import Twist
+from custom_interfaces.msg import ControlMessage
+from custom_interfaces.srv import AxisState
+from rclpy.qos import qos_profile_sensor_data
+from rcl_interfaces.msg import SetParametersResult
+from rclpy.qos import (
+    QoSProfile,
+    ReliabilityPolicy,
+    HistoryPolicy,
+    DurabilityPolicy,
+    LivelinessPolicy,
+    Duration,
+)
+
+#print(AxisState.CLOSED_LOOP_CONTROL) # 8
+#print(AxisState(8).name) # CLOSED_LOOP_CONTROL
+
+class TankDriveNode(Node):
+    def __init__(self):
+        super().__init__('tank_drive')
+        self.control_message_publisher = self.create_publisher(ControlMessage, '/odrive0/control_message', 10)
+        self.declare_parameter("max_vel",5.0)
+        self.cmd_vel_subscriber = self.create_subscription(Twist, 'cmd_vel', self.cmd_vel_callback, 10)
+        self.request_axis_state_client = self.create_client(AxisState, '/odrive_axis0/request_axis_state')
+        self.qos_publish = QoSProfile(
+            history=HistoryPolicy.KEEP_LAST,
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            depth=1,
+            durability=DurabilityPolicy.VOLATILE,
+            liveliness=LivelinessPolicy.AUTOMATIC,
+            lifespan=Duration(seconds=0.1, nanoseconds=0),
+        )
+        self.ctrl_msg_qos = QoSProfile(history=HistoryPolicy.KEEP_ALL)
+        
+        self.qos_sub = QoSProfile(
+            history=HistoryPolicy.KEEP_ALL
+        )
+
+    def cmd_vel_callback(self, msg):
+        speed = ControlMessage()
+        speed.control_mode = 2
+        speed.input_mode = 1
+        speed.input_vel = msg.linear.x * self.get_parameter("max_vel").value;
+        if msg.angular.z == 1:
+            self.request_axis_state("IDLE")
+        elif msg.angular.x == 1:
+            self.request_axis_state("CLOSED_LOOP_CONTROL")
+
+        self.control_message_publisher.publish(speed)
+
+    def request_axis_state(self, state):
+        req = AxisState.Request()
+        if state == "IDLE":
+            req.axis_requested_state = 1
+            future = self.request_axis_state_client.call_async(req)
+            future.add_done_callback(self.check_success)
+        elif state == "CLOSED_LOOP_CONTROL":
+            req.axis_requested_state = 8
+            future = self.request_axis_state_client.call_async(req)
+            future.add_done_callback(self.check_success)
+
+        
+        
+      
+        
+        
+
+    def check_success(self,req):
+        if req.procedure_result == 0 or req.procedure_result == 1:
+            print("success")
+        else:
+            print("failure")
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    tank_drive_mode = TankDriveNode()
+
+    rclpy.spin(tank_drive_mode)
+    rclpy.shutdown()
+
+   
+if __name__ == '__main__':
+    main()
---
+++ onboarding_ws/src/onboarding_ws_package_brc/onboarding_ws_package_brc/talker.py
@@ -0,0 +1,55 @@
+import rclpy # node class
+from rclpy.node import Node
+
+from std_msgs.msg import String
+from pcl_msgs.srv import UpdateFilename
+
+class Talker(Node): # Talker Subclass of Node class
+
+    def __init__(self):
+        super().__init__('talker') # Initializes a node called talker in Node Super
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        timer_period = 0.5  # seconds
+        self.client = self.create_client(UpdateFilename, 'name')
+
+        self.timer1 = self.create_timer(timer_period, self.publisher_callback)
+        self.timer2 = self.create_timer(timer_period, self.change_name_request)
+        self.i = 0
+        self.declare_parameter("Name", "Kamil")
+
+    def publisher_callback(self):
+        msg = String()
+        msg.data = str(self.i)
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
+
+
+    def change_name_request(self):
+        self.name = self.get_parameter("Name").value
+        req = UpdateFilename.Request()
+        req.filename = self.name
+        future = self.client.call_async(req)
+        future.add_done_callback(self.check_success)
+
+    def check_success(self,future):
+        if not future.result().success:
+            self.get_logger().info("Name Change Failed")        
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    talker = Talker()
+
+    rclpy.spin(talker)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    talker.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
